3d9c3e052b5a517413c42a7f2a864b3292c411ee
u19340266 <62009914+u19340266@users.noreply.github.com>
2020-03-10 11:47:03 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+$('document').ready(function() {
+	$('#u19340266').click().alert(''INF 272 is going to be the best module this year! Coding is a form of art that uses logic and maths rather than paint and brushes. Magnificent works of art can be created with the ease of coding.");
+	
+});
